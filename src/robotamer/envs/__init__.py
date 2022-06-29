9416dc5a5f676a616dd1231a48155d2cb0686711
rgarciap <rjgpinel@gmail.com>
2022-06-30 01:03:04 +0200

    envs/__init__: update env_name to avoid muse conflicts + remove DR name

--- src/robotamer/envs/__init__.py
+++ src/robotamer/envs/__init__.py
@@ -2,13 +2,13 @@ from gym.envs.registration import register
 
 envs = [
     dict(
-        id="Pick-v0",
+        id="RealRobot-Pick-v0",
         entry_point="robotamer.envs.pick:PickEnv",
         max_episode_steps=150,
         reward_threshold=1.0,
     ),
     dict(
-        id="Push-v0",
+        id="RealRobot-Push-v0",
         entry_point="robotamer.envs.push:PushEnv",
         max_episode_steps=600,
         reward_threshold=1.0,
@@ -18,10 +18,4 @@ envs = [
 
 for env_dict in envs:
     name = env_dict["id"]
-    register(**env_dict)
-    # domain randomization
-    dr_name = f"DR-{name}"
-    env_dict = env_dict.copy()
-    env_dict["id"] = dr_name
-    env_dict["kwargs"] = dict(domain_randomization=True)
     register(**env_dict)
