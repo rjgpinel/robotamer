abfab31c7d97a56da6455e0543ede9614416e790
rjgpinel <rjgpinel@gmail.com>
2022-07-07 15:50:14 +0200

    envs/__init__: update with variations

--- src/robotamer/envs/__init__.py
+++ src/robotamer/envs/__init__.py
@@ -27,7 +27,16 @@ envs = [
     ),
 ]
 
+num_variants = {"RealRobot-PushButtons-v0": 5220, "RealRobot-Stack-v0": 200}
 
 for env_dict in envs:
     name = env_dict["id"]
     register(**env_dict)
+    if name in num_variants.keys():
+        num_v = num_variants[name]
+        for var in range(num_v):
+            var_env_dict = env_dict.copy()
+            clean_env_name = "-".join(name.split("-")[1:])
+            var_env_dict["id"] = f"RealRobot-Var{var}-{clean_env_name}"
+            register(**var_env_dict)
+
