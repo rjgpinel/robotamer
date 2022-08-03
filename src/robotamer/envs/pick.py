3e3ca5a383a16fab400b6f21c847530591c9fd5f
rjgpinel <rjgpinel@gmail.com>
2022-08-03 10:23:03 +0200

    envs/pick: update pose collection

--- src/robotamer/envs/pick.py
+++ src/robotamer/envs/pick.py
@@ -1,38 +1,44 @@
-from numpy import pi
+import rospy
+import time
 
+from numpy import pi
 from robotamer.envs.base import BaseEnv
 
+INIT_X = 0.085
+INIT_Y = 0.24
+CUBES_HEIGHT = 0.025*2
+CUBES_DIST = 0.17
+
+
 class PickEnv(BaseEnv):
-    INITIAL_XY = [-0.1125, 0.0]
-    CUBE_HEIGHT = 0.0225 * 2
-
-
     def __init__(self, cam_list, depth=False):
         super(PickEnv, self).__init__(cam_list=cam_list, depth=depth)
         self.safe_height = 0.1
-        self.clean_i = 0
+        self.set_counter = 0
 
-    def set_scene(self, sim_env):
+    def set_scene(self, sim_env, initial_xy=[[-0.745, 0.225], [-0.745, 0.0], [-0.745, -0.225]]):
         sim_obs = sim_env.observe()
-        default_orn = [pi, 0, pi / 2]
-        cube_pos = sim_obs[f"cube0_pos"]
-        put_success = self.put([cube_pos[0], cube_pos[1], self.CUBE_HEIGHT/2], default_orn)
-        if not put_success:
-            raise RuntimeError("Setting scene failed")
+        num_cubes = 1 + sim_env.num_distractors
+        orn=[pi, 0, pi / 2]
+        for i in range(num_cubes):
+            x_get, y_get = initial_xy[i]
+            if self.set_counter == 0:
+                center_success = self.center_object_pos([x_get, y_get, CUBES_HEIGHT/2])
+            elif self.set_counter % 2 == 0:
+                    orn=[pi, 0, pi / 2]
+                    pick_success = self.pick([x_get, y_get, CUBES_HEIGHT / 2], orn)
+            else:
+                 orn = [pi, 0, 0]
+                 pick_success = self.pick([x_get, y_get,CUBES_HEIGHT / 2], orn)
 
+            cube_pos = sim_obs[f"cube{i}_pos"]
+            put_success = self.put(cube_pos, orn)
+            if not put_success:
+                raise RuntimeError("Setting scene failed")
+            if rospy.is_shutdown():
+                break
+        self.set_counter+=1
         return True
 
     def clean_scene(self, sim_env):
-        sim_obs = sim_env.observe()
-        x_get, y_get, _ = sim_obs["cube0_pos"]
-        if self.clean_i % 2 == 0:
-            default_orn = [pi, 0, pi / 2]
-        else:
-            default_orn = [pi, 0, 0]
-        self.clean_i += 1
-
-        pick_success = self.pick([x_get, y_get, self.CUBE_HEIGHT / 2], default_orn)
-        if not pick_success:
-            raise RuntimeError("Cleaning scene failed")
-
         return True
