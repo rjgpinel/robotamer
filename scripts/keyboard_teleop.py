5291cfa4969b5b999a9ccf5213bd975c52c02f4e
Minttu Alakuijala <minttu.alakuijala@gmail.com>
2022-08-08 14:33:59 +0200

    Add start, save and discard functionality to keyboard teleop script.

--- scripts/keyboard_teleop.py
+++ scripts/keyboard_teleop.py
@@ -4,7 +4,7 @@ import rospy
 from sensor_msgs.msg import Joy
 
 def talker():
-    pub = rospy.Publisher('joy', Joy, queue_size=1)
+    pub = rospy.Publisher('joy_teleop', Joy, queue_size=1)
     rospy.init_node('teleop', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
@@ -16,13 +16,17 @@ def talker():
         if key == 'w':
             cmd.axes = [0, 1, 0, 0, 0, 0]
         elif key == 'a':
-            cmd.axes = [0.5, 0, 0, 0, 0, 0]
+            cmd.axes = [1, 0, 0, 0, 0, 0]
         elif key == 's':
             cmd.axes = [0, -1, 0, 0, 0, 0]
         elif key == 'd':
-            cmd.axes = [-0.5, 0, 0, 0, 0, 0]
-        elif key == 'x':
+            cmd.axes = [-1, 0, 0, 0, 0, 0]
+        elif key == 'A':
             cmd.buttons = [1] + [0] * 11
+        elif key == 'B':
+            cmd.buttons = [0] + [1] + [0] * 10
+        elif key == 'X':
+            cmd.buttons = [0, 0] + [1] + [0] * 9
         rospy.loginfo(cmd)
         pub.publish(cmd)
         rate.sleep()
