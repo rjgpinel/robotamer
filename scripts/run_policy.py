9ed609a14a081aefa2b863d37c6ce9aea3abb969
rjgpinel <rjgpinel@gmail.com>
2022-10-15 01:10:25 +0200

    scripts/run_policy: update to handle bigger span history

--- scripts/run_policy.py
+++ scripts/run_policy.py
@@ -24,6 +24,7 @@ from ibc.data.utils import (
     state_to_frames,
 )
 from ibc.model.factory import create_vision_model, create_bc_model
+from ibc.data.utils import flatten_normalize_dict, filter_state
 
 from muse.envs.utils import realsense_resize_crop, realsense_resize_batch_crop
 from muse.core.constants import REALSENSE_CROP, REALSENSE_RESOLUTION
@@ -51,6 +52,9 @@ def main(args):
     delay_hist = checkpoint["args"].delay_hist
     frame_hist = checkpoint["args"].frame_hist
     num_frames = len(frame_hist) + 1
+    max_frame_idx = max(frame_hist)
+    history_idx_list = [0] + frame_hist
+
     if not args.cam_list:
         args.cam_list = checkpoint["args"].cam_list
     else:
@@ -64,6 +68,8 @@ def main(args):
     normalization = "vit" if "vit" in checkpoint["args"].model else "resnet"
     norm_img = get_im_norm_stats(normalization)
     transform = create_transform(
+        args,
+        None,
         normalization=normalization,
         data_aug="",
     )
@@ -79,6 +85,7 @@ def main(args):
     model = create_bc_model(
         vision_model=vision_model,
         data_stats=checkpoint["stats"],
+        use_state=checkpoint["args"].state
     )
 
     model.load_state_dict(checkpoint["model"])
@@ -100,7 +107,7 @@ def main(args):
     render_w, render_h = REALSENSE_RESOLUTION
     W, H = REALSENSE_CROP
     frames = torch.zeros(
-        (num_streams, num_frames, H, W, 3), dtype=torch.float32, device=ptu.device
+        (num_streams, max_frame_idx + 1, H, W, 3), dtype=torch.float32, device=ptu.device
     )
 
     step_frames = torch.zeros((num_streams, render_h, render_w, 3), dtype=torch.uint8)
@@ -118,15 +125,21 @@ def main(args):
         )
         if i == 0:
             frames[:, :-1] = frames[:, -1].unsqueeze(1)
-        frames_norm = frames.unsqueeze(0) / 255
+        frames_select_idx = [max_frame_idx - i for i in reversed(history_idx_list)]
+        frames_norm = frames[:, frames_select_idx].unsqueeze(0) / 255
         frames_norm = transform(frames_norm)
 
         if rospy.is_shutdown():
             break
 
+        state = None
+        if checkpoint["args"].state:
+            state = filter_state(obs)
+            state = flatten_normalize_dict(state, checkpoint["stats"]["traj_stats"], select_keys=["gripper_pos", "gripper_theta"]).unsqueeze(0).to(ptu.device)
+
         # Compute action
         with torch.no_grad():
-            action = model(frames_norm)
+            action = model(frames_norm, state)
 
         # Post-process action
         action = model.process_output(action)
